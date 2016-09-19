b11013ee46954a871c0fe01c861a64d25a673cc1
Vlad <vladt9@yandex.ru>
2016-09-20 00:34:56 +0300

    Implemented Sorter logic

--- SortCompetitionProject/src/main/java/sort_algs/Sorter.java
+++ SortCompetitionProject/src/main/java/sort_algs/Sorter.java
@@ -4,8 +4,46 @@ package sort_algs;
  * Created by VladVin on 19.09.2016.
  */
 public class Sorter {
+    private enum Order { ASCENDING, DESCENDING, RANDOM, NONE }
+
     public double[] sort(double[] array) {
-        // TODO: Analyze data and choose sorting algorithm
-        return null;
+        // Determine order of elements in array
+        Order order = Order.NONE;
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] <= array[i + 1]) {
+                if (order == Order.ASCENDING || order == Order.NONE) {
+                    order = Order.ASCENDING;
+                } else {
+                    order = Order.RANDOM;
+                    break;
+                }
+            } else {
+                if (order == Order.DESCENDING || order == Order.NONE) {
+                    order = Order.DESCENDING;
+                } else {
+                    order = Order.RANDOM;
+                    break;
+                }
+            }
+        }
+
+        // Choose sorting algorithm depending on elements order
+        SortingAlgorithm sortAlg = null;
+        switch (order) {
+            case ASCENDING:
+                sortAlg = SortAlgFabric.createAlgorithm(SortAlgFabric.SortAlgType.TIM_SORT);
+                break;
+            case DESCENDING:
+                // TODO: Use radix sort algorithm
+                break;
+            case RANDOM:
+                // TODO: Use radix sort algorithm
+                break;
+            default:
+                return null;
+        }
+
+        // Sort array
+        return sortAlg.sort(array);
     }
 }
