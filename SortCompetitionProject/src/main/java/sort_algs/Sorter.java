9dd540582e1bdfb8e2bd3915dac3db9079a99e67
Vlad <vladt9@yandex.ru>
2016-09-19 20:15:32 +0300

    Added infrastructure for sorting algorithms building

---
+++ SortCompetitionProject/src/main/java/sort_algs/Sorter.java
@@ -0,0 +1,11 @@
+package sort_algs;
+
+/**
+ * Created by VladVin on 19.09.2016.
+ */
+public class Sorter {
+    public double[] sort(double[] array) {
+        // TODO: Analyze data and choose sorting algorithm
+        return null;
+    }
+}
