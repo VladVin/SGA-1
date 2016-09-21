257377582e41c53027838d023524c56ab317117f
Vlad <vladt9@yandex.ru>
2016-09-21 23:51:01 +0300

    Updated Sorter

--- SortCompetitionProject/src/main/java/sort_algs/Sorter.java
+++ SortCompetitionProject/src/main/java/sort_algs/Sorter.java
@@ -4,28 +4,11 @@ package sort_algs;
  * Created by VladVin on 19.09.2016.
  */
 public class Sorter {
-    private enum Order { ASCENDING, DESCENDING, RANDOM, NONE }
+    public enum Order { ASCENDING, DESCENDING, RANDOM, NONE }
 
     public double[] sort(double[] array) {
         // Determine order of elements in array
-        Order order = Order.NONE;
-        for (int i = 0; i < array.length - 1; i++) {
-            if (array[i] <= array[i + 1]) {
-                if (order == Order.ASCENDING || order == Order.NONE) {
-                    order = Order.ASCENDING;
-                } else {
-                    order = Order.RANDOM;
-                    break;
-                }
-            } else {
-                if (order == Order.DESCENDING || order == Order.NONE) {
-                    order = Order.DESCENDING;
-                } else {
-                    order = Order.RANDOM;
-                    break;
-                }
-            }
-        }
+        Order order = determineOrder(array);
 
         // Choose sorting algorithm depending on elements order
         SortingAlgorithm sortAlg = null;
@@ -45,5 +28,32 @@ public class Sorter {
 
         // Sort array
         return sortAlg.sort(array);
+    }
+
+    public Order determineOrder(double[] array) {
+        Order order = Order.NONE;
+        for (int i = 0; i < array.length - 1; i++) {
+            double diff = array[i + 1] - array[i];
+            if (diff == 0.0) {
+                continue;
+            }
+            if (diff > Double.MIN_VALUE) {
+                if (order == Order.ASCENDING || order == Order.NONE) {
+                    order = Order.ASCENDING;
+                } else {
+                    order = Order.RANDOM;
+                    break;
+                }
+            } else {
+                if (order == Order.DESCENDING || order == Order.NONE) {
+                    order = Order.DESCENDING;
+                } else {
+                    order = Order.RANDOM;
+                    break;
+                }
+            }
+        }
+
+        return order;
     }
 }
